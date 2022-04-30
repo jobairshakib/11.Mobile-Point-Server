8c9042b954fbae4aa865675d548ab5ffb6f1bc2e
Jobair Shakib <shakib.jobair@gmail.com>
2022-04-30 13:16:22 +0600

    addItem added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+require('dotenv').config()
+const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://mpuser:PhDL5CxnH9yHjq8E@cluster0.9tf8i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9tf8i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -19,6 +20,19 @@ async function run() {
             const cursor = itemCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
+        })
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        })
+
+        app.post('/addItem', async (req, res) => {
+            const item = req.body;
+            const result = await itemCollection.insertOne(item);
+            res.send({success:"Item Added"});
         })
 
 
